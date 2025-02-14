0b1b03c3e2e00f685e8006e9c5106a16b63ff912
Ondrej Sedlacek <xsedla1o@stud.fit.vutbr.cz>
2025-02-14 13:22:11 +0100

    feat: introduce one-class SVM demo

---
+++ oc_svm_demo.py
@@ -0,0 +1,24 @@
+from loglizer.loglizer.models import OneClassSMV
+from loglizer.loglizer import dataloader, preprocessing
+
+struct_log = 'data/HDFS.csv'  # The structured log file
+label_file = 'data/anomaly_label.csv'  # The anomaly label file
+
+if __name__ == '__main__':
+    (x_train, y_train), (x_test, y_test) = dataloader.load_HDFS(struct_log,
+                                                                label_file=label_file,
+                                                                window='session',
+                                                                train_ratio=0.5,
+                                                                split_type='uniform')
+    feature_extractor = preprocessing.FeatureExtractor()
+    x_train = feature_extractor.fit_transform(x_train, term_weighting='tf-idf')
+    x_test = feature_extractor.transform(x_test)
+
+    model = OneClassSMV()
+    model.fit(x_train[y_train == 0, :])  # Use only normal samples for training
+
+    print('Train validation:')
+    precision, recall, f1 = model.evaluate(x_train, y_train)
+
+    print('Test validation:')
+    precision, recall, f1 = model.evaluate(x_test, y_test)
